5068eb664735706df3e0a406ad9b3c1cd73e7685
shinji510 <87000776+shinji510@users.noreply.github.com>
2024-11-15 01:43:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function showOrHide() {//パスワードを表示するか隠すかを判断す�
     { name: "user1", age: 21, email: "user1@example.com", password: "user-1" },
     { name: "user2", age: 22, email: "user2@example.com", password: "user-2" },
     { name: "user3", age: 23, email: "user3@example.com", password: "user-3" },
-    { name: "shinji", age: 25, email: "shinji.riku@gmail.com", password: "shinji0510" },
+    { name: "shinji4", age: 25, email: "shinji.riku@gmail.com", password: "shinji0510" }
   ];
   
   function login() {
